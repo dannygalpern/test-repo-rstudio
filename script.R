f81eddc6deaa25ee955c86fb90b9937817f4a29f
dannygalpern <83598167+dannygalpern@users.noreply.github.com>
2023-02-06 13:29:24 -0500

    Create script.R

---
+++ script.R
@@ -0,0 +1,24 @@
+#Purpose: Learning GitHub
+
+install.packages("usethis")
+library(usethis)
+
+#set usename and email from GitHub account
+use_git_config(user.name = "dannygalpern", use.email = "dannygalpern@gmail.com")
+
+#get token
+create_github_token()
+#PAT personal access token = "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"
+
+#set token below in console
+gitcreds::gitcreds_set()
+
+#create new repository on GitHub
+#click "<> Code" button and copy HTTPS .git link
+create_from_github(
+  #HTTPS .git link 
+  "https://github.com/dannygalpern/test-repo-rstudio.git",
+  #local location to clone 
+  destdir = "C:/Users/daniel.galpern/OneDrive - City of Philadelphia/Projects/GitHub")
+
+#new window should open with new R project
